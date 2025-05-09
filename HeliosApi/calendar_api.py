3c7378c6e84b3e069c2d80613f665eda46781197
Fastfive9 <louislecomte13@gmail.com>
2025-05-09 15:12:58 +0200

    Update calendar_api.py

--- HeliosApi/calendar_api.py
+++ HeliosApi/calendar_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -30,5 +31,5 @@ def get_today_calendar():
     })
 
 if __name__ == '__main__':
- import os
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+)))
